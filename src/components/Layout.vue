302270ef5f6d0ce742f06bb9e04055edb8ef8c74
lizhiqiu <lizhiqiu@baidu.com>
2023-04-19 23:40:46 +0800

    高度

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="layout">
-    <div class="title">
-      F1 Analysis
+    <div class="layoutBorder">
+    <div class="title">F1 Analysis</div>
+    <div class="author">@Yansu Wang</div>
+    <div class="layoutContent">
+      <div class="summary">
+        This system is mainly divided into three parts: Map, Driver's Standings,
+        and Constructor's Standings.
+      </div>
+      <div class="router">
+        <dis class="routerName">Map</dis>
+        <li>
+          Circuit: Views the global distribution of circuits and related
+          information.
+        </li>
+        <li>
+          Driver's Nationality: A heat map shows the distribution of drivers'
+          nationalities worldwide.
+        </li>
+      </div>
+      <div class="router">
+        <dis class="routerName">Driver's Standings</dis>
+        <li>
+          Top 30 in History: This filters out the top 30 drivers who have won
+          the most championships from 1950 to 2023.
+        </li>
+        <li>
+          By Year: Views the driver standings by points for each season from
+          year 2014-2023.
+        </li>
+      </div>
+      <div class="router">
+        <dis class="routerName">Constructor's Standings</dis>
+        <li>
+          Points: This compares the total historical points and average race
+          points of constructors.
+        </li>
+        <li>
+          By Year: Views the constructor standings by points for each season
+          from year 2014-2023.
+        </li>
+      </div>
+      <div class="router">
+        <li>
+          Data source: Ergast and Kaggle
+        </li>
+        <li>
+          Data processing: Java script and Python
+        </li>
+      </div>
     </div>
-    <div class="author">
-      @Yansu Wang
     </div>
-    <p>
-      test
-    </p>
   </div>
 </template>
 
@@ -26,12 +68,34 @@ export default {
 }
 </script>
 <style scoped="less">
-  .layout {
-    width: 100%;
-    height: 100%;
-    text-align: center;
-  }
-  .title {
-    font-size: 100px;
-  }
+.layout {
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
+}
+.layoutBorder {
+    width: 800px;
+    border: 1px soild #fff;
+}
+.author {
+  text-align: center;
+}
+.title {
+  font-size: 100px;
+  text-align: center;
+}
+
+.layoutContent {
+  margin-top: 20px;
+  line-height: 40px;
+}
+
+.router {
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.routerName {
+  font-size: 25px;
+}
 </style>
